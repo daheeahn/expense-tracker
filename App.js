47471dd4f82eb42984433d189b05936a5e812540
daheeahn <deg9810@gmail.com>
2024-10-14 02:46:25 -0700

    set code base for navigating page

---
+++ App.js
@@ -0,0 +1,55 @@
+const routes = [
+  {
+    path: "/",
+    sectionId: "mainSection",
+  },
+  {
+    path: "/add",
+    sectionId: "addSection",
+  },
+  {
+    path: "/report",
+    sectionId: "reportSection",
+  },
+];
+
+const navigate = (url) => {
+  window.history.pushState(null, null, url);
+  App();
+};
+
+const App = async () => {
+  const pageMatches = routes.map((route) => {
+    return {
+      route: route,
+      isMatch: window.location.pathname === route.path,
+    };
+  });
+
+  const match = pageMatches.find((pageMatch) => pageMatch.isMatch);
+
+  // Hide all sections
+  document.querySelectorAll(".section").forEach((section) => {
+    section.style.display = "none";
+  });
+
+  if (match) {
+    document.getElementById(match.route.sectionId).style.display = "block";
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  // console.log("😎 DOMContentLoaded");
+  document.body.addEventListener("click", (e) => {
+    const target = e.target.closest("a");
+    console.log("target", target);
+    if (!(target instanceof HTMLAnchorElement)) return;
+
+    e.preventDefault();
+    navigate(target.href);
+  });
+
+  App();
+});
+
+window.addEventListener("popstate", App);
--- script.js
+++ script.js
@@ -1,84 +1,58 @@
-// Initial Data
-let tableEntries = [
-  { type: "shopping", memo: "Buy some grocery", amount: 25000 },
-  { type: "subscription", memo: "Netflix", amount: 18000 },
-  { type: "food", memo: "Buy a ramen", amount: 20000 },
-  { type: "transportation", memo: "Buy a compass card", amount: 30000 },
+let expenses = [
+  {
+    id: 1,
+    category: "Shopping",
+    description: "Buy some grocery",
+    amount: 120,
+    date: "2024-10-13",
+  },
+  {
+    id: 2,
+    category: "Subscription",
+    description: "Disney+ Annual",
+    amount: 80,
+    date: "2024-10-13",
+  },
+  {
+    id: 3,
+    category: "Food",
+    description: "Buy a ramen",
+    amount: 32,
+    date: "2024-10-13",
+  },
+  {
+    id: 4,
+    category: "Transportation",
+    description: "Charging Tesla",
+    amount: 18,
+    date: "2024-10-12",
+  },
+  {
+    id: 5,
+    category: "Transportation",
+    description: "Charging Tesla",
+    amount: 18,
+    date: "2024-10-12",
+  },
 ];
 
-// Function to update data expense summary
-function updateSummary() {
-  let totalExpense = tableEntries.reduce((ex, e) => {
-    ex += e.amount;
-    return ex;
-  }, 0);
-  updatedExp.innerText = totalExpense;
-}
-
-// Function to add new entry to the dataset and expense table
-function addItem() {
-  let type = itemType.value;
-  let memo = document.getElementById("memo");
-  let amount = document.getElementById("amount");
-  let date = document.getElementById("date");
-
-  // Input validation
-  if (memo.value === "") return alert("Put Memo");
-  if (Number(amount.value) === 0) return alert("Put Amount");
-  if (Number(amount.value) <= 0)
-    return alert("Incorrect amount! can't add negative");
-
-  // Push new data
-  tableEntries.push({
-    type: type,
-    memo: memo.value,
-    amount: Number(amount.value),
-    date: date.value,
+function renderExpenses() {
+  const todayList = document.querySelector(".expenses-list ul");
+  todayList.innerHTML = "";
+  expenses.forEach((expense) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <div class="expense-item">
+        <div class="details">
+          <p class="category">${expense.category}</p>
+          <p class="description">${expense.description}</p>
+        </div>
+        <div class="amount">- $${expense.amount}</div>
+      </div>
+    `;
+    todayList.appendChild(li);
   });
-
-  updateTable();
-  memo.value = "";
-  amount.value = 0;
 }
 
-// Function to load all entry in the expense table
-function loadItems(e, i) {
-  let table = document.getElementById("table");
-  let row = table.insertRow(i + 1);
-  let cell0 = row.insertCell(0);
-  let cell1 = row.insertCell(1);
-  let cell2 = row.insertCell(2);
-  let cell3 = row.insertCell(3);
-  let cell4 = row.insertCell(4);
-  cell0.innerHTML = i + 1;
-  cell1.innerHTML = e.memo;
-  cell2.innerHTML = e.amount;
-  cell3.innerHTML = e.type;
-  cell4.innerHTML = "&#9746;";
-  cell4.classList.add("zoom");
-  cell4.addEventListener("click", () => del(e));
-}
-
-// Clear the table before updation
-function remove() {
-  while (table.rows.length > 1) table.deleteRow(-1);
-}
-
-// Function to delete a specific entry
-function del(el) {
-  remove();
-  tableEntries = tableEntries.filter((e) => e.name !== el.name);
-  tableEntries.map((e, i) => loadItems(e, i));
-  updateSummary();
-}
-
-// To render all entries
-function updateTable() {
-  remove();
-  tableEntries.map((e, i) => {
-    loadItems(e, i);
-  });
-  updateSummary();
-}
-
-updateTable();
+// 초기에 리스트 렌더링
+renderExpenses();
