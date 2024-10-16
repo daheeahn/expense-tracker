521ee667d9b4013ba788d524ed864c5c1e56c16f
daheeahn <deg9810@gmail.com>
2024-10-16 16:06:58 -0700

    add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "base-light-light-80": "#FCFCFC",
+        "base-dark-dark-25": "#292B2D",
+        "base-light-light-20": "#91919F",
+        "red-red-100": "#FD3C4A",
+      },
+    },
+  },
+  plugins: [],
+};
